cb2fedc1c30fcec23edb4712403cb9c36bec6c1e
Aaron Langlitz <aaron@wokiex.com>
2020-05-05 14:17:18 +0200

    404 Error Handling

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
 const server = app.listen(3000, (req, res) => {
     console.log("Server started on port", server.address().port);
 });
@@ -10,3 +9,6 @@ const server = app.listen(3000, (req, res) => {
 app.get("/", (req, res) => {
     res.sendFile("index.html");
 });
+app.get('*', function(req, res){
+    res.send('Page Not Found 404', 404);
+  });
